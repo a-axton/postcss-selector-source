8c945141eb98c82fe2389db34f76edb1eafd6571
a-axton <dizzysfarm@gmail.com>
2016-01-10 20:22:59 -0800

    switch source maps to postcss implementation

--- src/Source.js
+++ src/Source.js
@@ -1,21 +1,6 @@
-import fs from 'fs';
-import sourceMap from 'source-map';
-import convert from 'convert-source-map';
-
-class Source {
+export default class Source {
   constructor() {
     this.smc = null;
-  }
-
-  setSourceMap(sourceMapURLComment) {
-    if (sourceMapURLComment.indexOf('base64') > -1) {
-      let map = convert.fromComment(sourceMapURLComment).toObject();
-      this.smc = new sourceMap.SourceMapConsumer(map);
-    } else {
-      let sourceMapURL = sourceMapURLComment.split('=').pop();
-      let map = fs.readFileSync(sourceMapURL).toString();
-      this.smc = new sourceMap.SourceMapConsumer(JSON.parse(map));
-    }
   }
 
   getOriginalPosition(start, end) {
@@ -29,11 +14,7 @@ class Source {
   }
 
   getPosition(line, column) {
-    return this.smc.originalPositionFor({
-      line: line,
-      column: column
-    });
+    let results = this.smc.origin(line, column);
+    return results ? results : null;
   }
 }
-
-export default Source;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 import postcss from 'postcss';
-import { Scouter } from 'scouter';
 import Source from './Source';
 
 let source = new Source();
-let scouter = new Scouter();
 let selectors = [];
-let removeAbove = {
-  line: 0,
-  set: function(line) {
-    this.line = line;
-  }
-};
 
-// maps out the relevant css declaration info
-function _buildDecls(decls) {
+function buildDecls(decls) {
   return decls.map(decl => {
     let start = decl.source.start;
     let end = decl.source.end;
@@ -28,14 +19,12 @@ function _buildDecls(decls) {
     if (source.smc) {
       entry.originalPosition = source.getOriginalPosition(start, end);
     }
-
     return entry;
   });
 }
 
-function _buildMediaParams(params) {
+function buildMediaParams(params) {
   params = params.match(/\((.*?)\)/g);
-
   if (params && params.length) {
     return params.map(param => {
       param = param.replace(/[\])(]/g, '').replace(/\s/g, '').split(':');
@@ -47,56 +36,39 @@ function _buildMediaParams(params) {
   }
 }
 
-function _buildRuleEntry(rule) {
-  if (rule.source.start.line < removeAbove.line) {return;}
-
+function buildRuleEntry(rule) {
   let start = rule.source.start;
   let end = rule.source.end;
-  let decls = _buildDecls(rule.nodes);
+  let decls = buildDecls(rule.nodes);
   let entry = {
     selector: rule.selector,
     selectors: rule.selectors,
-    specificity: scouter.score(rule.selector),
     decls: decls,
     start: start,
     end: end
   };
-
   if (rule.parent.name === 'media') {
-    entry.params = _buildMediaParams(rule.parent.params);
+    entry.params = buildMediaParams(rule.parent.params);
   }
-
   if (source.smc) {
     entry.originalPosition = source.getOriginalPosition(start, end);
   }
-
   selectors.push(entry);
 }
 
 export default postcss.plugin('selector-source', (callback) => {
   selectors = [];
-  // logs each selector with startend position
-  return function(css, result) {
-    css.walkComments(function(comment) {
-      if (comment.text.indexOf('!ATTN') > -1) {
-        removeAbove.set(comment.source.start.line);
-      } else if (comment.text.indexOf('sourceMappingURL') > -1) {
-        source.setSourceMap(comment.text);
-      }
-    });
-
-    css.walkRules(_buildRuleEntry);
-
+  return (css, result) => {
+    source.smc = css.source.input;
+    css.walkRules(buildRuleEntry);
     if (!source.smc) {
       result.warn('make sure an external css source-map is being generated');
     }
-
     if (callback && typeof(callback) === 'function') {
       callback(selectors);
     } else {
       result.warn('provide a callback to see the results');
     }
-
     return result;
   };
 });
